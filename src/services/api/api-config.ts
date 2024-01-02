9f450c7cd66e992638fb0cecbe19656e6bd46473
lidordvir <lidor4001@gmail.com>
2024-01-02 18:07:18 +0200

    * Created api routes

---
+++ src/services/api/api-config.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+export const BASE_URL = 'http://localhost:3000';
+
+export const todoListAxios = axios.create({
+  baseURL: BASE_URL,
+  validateStatus: (status: number) => status < 300,
+  timeout: 10000,
+});
+
+axiosRetry(todoListAxios, {
+  retries: 5,
+  retryDelay: (retryCount: number) => {
+    return 500 * Math.pow(2, retryCount - 1);
+  },
+});
---
+++ src/services/api/api.ts
@@ -0,0 +1,79 @@
+import {ITask, ITaskStatus} from '../../utills/types';
+import {todoListAxios} from './api-config';
+
+export interface IGetTasksResponse {
+  tasks: ITask[];
+}
+
+export async function getTasks({
+  page,
+  limit,
+}: {
+  page: number;
+  limit: number;
+}): Promise<IGetTasksResponse> {
+  const res = await todoListAxios({
+    method: 'GET',
+    url: '/tasks',
+    params: {
+      page,
+      limit,
+    },
+  });
+  return res.data;
+}
+
+export async function createTask({
+  task,
+}: {
+  task: {
+    title: string;
+    description: string;
+  };
+}): Promise<ITask> {
+  const res = await todoListAxios({
+    method: 'POST',
+    url: '/task/create',
+    data: {
+      ...task,
+    },
+  });
+  return res.data;
+}
+
+export async function updateTask({
+  updateTaskProps,
+  taskId,
+}: {
+  updateTaskProps: {
+    title?: string;
+    description?: string;
+    status?: ITaskStatus;
+  };
+  taskId: string;
+}): Promise<ITask> {
+  const res = await todoListAxios({
+    method: 'PATCH',
+    url: `/task/${taskId}/update`,
+    data: {
+      ...updateTaskProps,
+    },
+  });
+  return res.data;
+}
+
+export interface IDeleteTaskResponse {
+  message: string;
+}
+
+export async function deleteTask({
+  taskId,
+}: {
+  taskId: string;
+}): Promise<IDeleteTaskResponse> {
+  const res = await todoListAxios({
+    method: 'DELETE',
+    url: `/task/${taskId}/delete`,
+  });
+  return res.data;
+}
